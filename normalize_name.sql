ccb9180dc9f2192accbdfc3b97fb9de28e59442f
xxswarpxx <antonemrrimt@gmail.com>
2026-02-27 13:05:15 -0500

    Add files via upload

---
+++ normalize_name.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION ebg_qa.normalize_name(name text)
+RETURNS text AS $$
+BEGIN
+    name := lower(name); -- Convert to lowercase
+    name := replace(name, '.', ''); -- Remove periods
+    name := replace(name, ',', ''); -- Remove commas
+    RETURN array_to_string(
+        ARRAY(SELECT DISTINCT unnest(string_to_array(name, ' ')) ORDER BY 1),
+        ' '
+    );
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
