d9d39428c42cdc6b6966326086bb67a3dfca98b3
xxswarpxx <antonemrrimt@gmail.com>
2026-02-27 12:27:53 -0500

    Add files via upload

---
+++ get_state_abbreviation.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION get_state_abbreviation(state_name text)
+RETURNS text AS $$
+DECLARE
+  abbreviation text;
+BEGIN
+  SELECT abb INTO abbreviation
+  FROM (VALUES
+    ('Alabama', 'AL'), ('Alaska', 'AK'), ('Arizona', 'AZ'), ('Arkansas', 'AR'),
+    ('California', 'CA'), ('Colorado', 'CO'), ('Connecticut', 'CT'),
+    ('Delaware', 'DE'), ('Florida', 'FL'), ('Georgia', 'GA'),
+    ('Hawaii', 'HI'), ('Idaho', 'ID'), ('Illinois', 'IL'), ('Indiana', 'IN'),
+    ('Iowa', 'IA'), ('Kansas', 'KS'), ('Kentucky', 'KY'), ('Louisiana', 'LA'),
+    ('Maine', 'ME'), ('Maryland', 'MD'), ('Massachusetts', 'MA'),
+    ('Michigan', 'MI'), ('Minnesota', 'MN'), ('Mississippi', 'MS'),
+    ('Missouri', 'MO'), ('Montana', 'MT'), ('Nebraska', 'NE'),
+    ('Nevada', 'NV'), ('New Hampshire', 'NH'), ('New Jersey', 'NJ'),
+    ('New Mexico', 'NM'), ('New York', 'NY'), ('North Carolina', 'NC'),
+    ('North Dakota', 'ND'), ('Ohio', 'OH'), ('Oklahoma', 'OK'),
+    ('Oregon', 'OR'), ('Pennsylvania', 'PA'), ('Rhode Island', 'RI'),
+    ('South Carolina', 'SC'), ('South Dakota', 'SD'), ('Tennessee', 'TN'),
+    ('Texas', 'TX'), ('Utah', 'UT'), ('Vermont', 'VT'), ('Virginia', 'VA'),
+    ('Washington', 'WA'), ('West Virginia', 'WV'), ('Wisconsin', 'WI'),
+    ('Wyoming', 'WY')
+  ) AS state_table(name, abb)
+  WHERE name = state_name;
+
+  RETURN abbreviation;
+END;
+$$ LANGUAGE plpgsql;
